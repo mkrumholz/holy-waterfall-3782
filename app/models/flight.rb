557b74aff3b8aeef84e40a13d79419a0c7094c3f
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-11 08:53:53 -0600

    Add airline names to flights index

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :bookings
   has_many :passengers, through: :bookings
+
+  def airline_name
+    airline.name
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  it {should belong_to :airline}
-  it {should have_many :bookings}
-  it {should have_many(:passengers).through(:bookings)}
+  describe 'relationships' do
+    it {should belong_to :airline}
+    it {should have_many :bookings}
+    it {should have_many(:passengers).through(:bookings)}
+  end 
+  
+  describe 'instance methods' do
+    before :each do
+      @airline_1 = Airline.create!(name: 'Icelandair')
+      
+      @flight_1 = @airline_1.flights.create!(number: "1234", date: Date.parse('2021-06-01'), departure_city: 'New York', arrival_city: 'Reykjavik')
+    end  
+
+    describe '#airline_name' do
+      it 'returns the name of the flight\'s airline' do
+        expect(@flight_1.airline_name).to eq @airline_1.name
+      end
+    end
+  end
 end
