3156a2f4b1cda60ca1996796e1f1f918eefdb909
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-11 09:56:39 -0600

    Create airline/passenger relation

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,5 @@
 class Passenger < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :flights, through: :bookings
+  has_many :airlines, through: :flights
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Passenger do
   describe 'relationships' do
-  it {should have_many(:bookings).dependent(:destroy)}
+    it {should have_many(:bookings).dependent(:destroy)}
     it {should have_many(:flights).through(:bookings)}
+    it {should have_many(:airlines).through(:flights)}
   end
 end
