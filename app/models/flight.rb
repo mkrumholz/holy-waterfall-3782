f72f67a44087fd87a886ce3e9e0c5a44c8d338e8
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-11 09:22:19 -0600

    Create helper method for finding the flight/passenger booking

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,4 +6,14 @@ class Flight < ApplicationRecord
   def airline_name
     airline.name
   end
+
+  def booking_for(passenger_id)
+    passenger = Passenger.find(passenger_id)
+    booking = bookings.where(passenger: passenger)
+    if booking.present?
+      booking.pluck(:id)[0]
+    else 
+      "Error: No booking was found for #{passenger.name} on flight ##{number}."
+    end
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -12,11 +12,26 @@ RSpec.describe Flight, type: :model do
       @airline_1 = Airline.create!(name: 'Icelandair')
       
       @flight_1 = @airline_1.flights.create!(number: "1234", date: Date.parse('2021-06-01'), departure_city: 'New York', arrival_city: 'Reykjavik')
+
+      @passenger_1 = Passenger.create!(name: 'JJ', age: 33)
+      @passenger_2 = Passenger.create!(name: 'Molly', age: 31)
+
+      @booking_1 = Booking.create!(flight_id: @flight_1.id, passenger_id: @passenger_1.id)
     end  
 
     describe '#airline_name' do
       it 'returns the name of the flight\'s airline' do
         expect(@flight_1.airline_name).to eq @airline_1.name
+      end
+    end
+
+    describe '#booking_for' do
+      it 'returns the id of the booking for given passenger' do
+        expect(@flight_1.booking_for(@passenger_1.id)).to eq @booking_1.id
+      end
+
+      it 'returns an error if passenger has no booking on the flight' do
+        expect(@flight_1.booking_for(@passenger_2.id)).to eq "Error: No booking was found for #{@passenger_2.name} on flight ##{@flight_1.number}."
       end
     end
   end
